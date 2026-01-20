989c6924dc5bbd3b8211e79d36026fc6ef9657f5
Idyllac <victor.via7@gmail.com>
2026-01-20 16:55:24 +0100

    changes

--- index.js
+++ index.js
@@ -113,6 +113,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 
+
 /***********************
  *  MIDDLEWARE, SECURITY + STATIC FILES
  ***********************/
